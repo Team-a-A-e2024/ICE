064e1dc0b438bef06e0e4d212c720978a69f01fb
Frederik-BipBop <Bastiansen44@gmail.com>
2024-12-06 10:36:22 +0100

    removed redundant line and made it a user package

--- src/main/java/User/Dish.java
+++ src/main/java/User/Dish.java
@@ -8,10 +8,8 @@ public class Dish {
 protected Map<pl.coderion.model.Product, Float> ingredients;
 
 public Dish(Map<pl.coderion.model.Product, Float> ingredients) {
-
     this.ingredients = new HashMap<pl.coderion.model.Product, Float>();
-
-    }
+}
 
     public Map<Product, Float> getIngredients() {
         return ingredients;
