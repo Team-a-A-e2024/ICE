c89c9006f63f93220aacd088d31eec7d64b7c6ce
AlissaStorm <Alissaalidib@hotmail.com>
2024-12-06 11:41:04 +0100

    Database setup and login/signUp

--- src/main/java/Authorization.java
+++ src/main/java/Authorization.java
@@ -1,30 +1,118 @@
 import Model.User;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Authorization {
+
+    private String loadFullUser = "SELECT userName, password FROM Users";
+    private String loadUserName = "SELECT userName FROM Users";
 
 
 
     public boolean login(String userName, String password) {
         ArrayList<User> users = new ArrayList<>();
 
-        for(User u : users){
-            if(u.getUserName() == userName){
-                if(u.getPassword() == password) {
-                    System.out.println("Login successful");
-                    return true;
+
+        String connectionString = "jdbc:sqlite:C:\\Users\\Alissa\\IdeaProjects\\ICE\\identifier.sqlite";
+        try (Connection con = DriverManager.getConnection(connectionString)){
+            System.out.println("Connected to database");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(loadFullUser);
+
+            while (rs.next()) {
+                String name = rs.getString("userName");
+                String pass = rs.getString("password");
+
+                User user = new User(name, pass);
+                users.add(user);
+            }
+
+            if (users.isEmpty()) {
+                System.out.println("Login failed: No users found in the database.");
+                return false;
+            }
+
+            for(User u : users){
+                if(u.getUserName().equals(userName)){
+                    if(u.getPassword().equals(password)) {
+                        System.out.println("Login successful");
+                        return true;
+                    }
+                    else{
+                        System.out.println("password does not match");
+                        return false;
+                    }
                 }
                 else{
-                    System.out.println("password does not match");
+                    System.out.println("Username does not match");
                     return false;
                 }
             }
-            else{
-                System.out.println("Username does not match");
-                return false;
-            }
+            stmt.close();
+            con.close();
+            return false;
+
+        }
+
+        catch (SQLException e) {
+            e.printStackTrace();
         }
         return false;
     }
+
+
+//    public boolean signUp(String userName, String password) {
+//        ArrayList<User> users = new ArrayList<>();
+//
+//        String connectionString = "jdbc:sqlite:identifier.sqlite";
+//        try(Connection con = DriverManager.getConnection(connectionString)){
+//
+//            System.out.println("Connected to database");
+//
+//            Statement stmt = con.createStatement();
+//            ResultSet rs = stmt.executeQuery(loadUserName);
+//
+//            while (rs.next()) {
+//                String name = rs.getString("userName");
+//
+//
+//                User user = new User(userName);
+//                users.add(user);
+//            }
+//
+//            for(User u : users){
+//                if(u.getUserName().equals(userName)){
+//                    System.out.println("Username already exists");
+//                    return false;
+//                }
+//                else{
+//                    String save = "INSERT INTO Users (userName, password) VALUES (?, ?)";
+//                    PreparedStatement ps = con.prepareStatement(save);
+//                    ps.setString(1, userName);
+//                    ps.setString(2, password);
+//
+//                    int rowInserted = ps.executeUpdate();
+//
+//                    //Checks if data is inserted in database
+//                    if(rowInserted > 0){
+//                        System.out.println("Data inserted successfully");
+//                    }
+//
+//                    ps.close();
+//                    con.close();
+//                    stmt.close();
+//
+//                    System.out.println("Signup successful");
+//                    return true;
+//                }
+//            }
+//
+//        }
+//        catch(SQLException e){
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Authorization auth = new Authorization();
+        auth.login("Alissa","123456");
     }
 }
