c06b53d8387aa4f56eb00e8250d35453fa6b33dd
Iirne <emil@caerli.dk>
2024-12-06 09:51:16 +0100

    Added user class with getters and setters

---
+++ src/main/java/user/User.java
@@ -0,0 +1,45 @@
+package user;
+
+
+import pl.coderion.model.Product;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class User {
+
+    private Set<String> Allergens;
+    private Set<Dish> Dishes;
+    private Set<Product> Products;
+
+    public Set<String> getAllergens() {
+        return Allergens;
+    }
+
+    public void setAllergens(Set<String> allergens) {
+        Allergens = allergens;
+    }
+
+    public Set<Dish> getDishes() {
+        return Dishes;
+    }
+
+    public void setDishes(Set<Dish> dishes) {
+        Dishes = dishes;
+    }
+
+    public void addDishes(Dish dish) {
+        Dishes.add(dish);
+    }
+
+    public Set<Product> getProducts() {
+        return Products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        Products = products;
+    }
+    public void addProduct(Product product) {
+        Products.add(product);
+    }
+}
