3eb882a45a85d82ed5fdf71c2b905062e4e4389b
Iirne <emil@caerli.dk>
2024-12-09 12:22:56 +0100

    minor changes to allow images to be input on their own

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import barscanner.Barscanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Barscanner.readQR("docs/TestCodes/IMG_8750.jpg"));
+        System.out.println("hello world");
     }
 }
