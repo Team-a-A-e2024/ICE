b3c85635419041e9c5193137926378bccfd5f9a1
Iirne <emil@caerli.dk>
2024-12-09 12:20:22 +0100

    minor changes to allow images to be input on their own

--- src/main/java/barscanner/Barscanner.java
+++ src/main/java/barscanner/Barscanner.java
@@ -1,6 +1,8 @@
 package barscanner;
 
 //java imports
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,24 +18,31 @@ public class Barscanner {
     //code source: https://www.geeksforgeeks.org/how-to-generate-and-read-qr-code-with-java-using-zxing-library/
     //minorly modified for usage
     // Function to read the QR file
-    public static String readQR(String path)
+    public static String readQR(BufferedImage image)
 
     {
         try {
-            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(path)))));
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(image)));
 
             Result result = new MultiFormatReader().decode(binaryBitmap);
 
             return result.getText();
         }
+        catch (NotFoundException e){
+            TextUI.displayMsg("error, Code not found:" + e.getMessage());
+        }
+        return null;
+    }
+
+    public static String readCodeFromPath(String path){
+        try {
+            return readQR(ImageIO.read(new FileInputStream(path)));
+        }
         catch (FileNotFoundException e){
-            TextUI.displayMsg("File not found");
+            TextUI.displayMsg("File not found:" + e.getMessage());
         }
         catch (IOException e){
-            TextUI.displayMsg("error");
-        }
-        catch (NotFoundException e){
-            TextUI.displayMsg("error, Code not found");
+            TextUI.displayMsg("IO exception:" + e.getMessage());
         }
         return null;
     }
