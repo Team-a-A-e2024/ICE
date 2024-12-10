22d8194eaff44c06f5d9caab18b10aa0d5f04b5e
Iirne <emil@caerli.dk>
2024-12-10 18:16:13 +0100

    updates

--- src/main/java/util/ApiService.java
+++ src/main/java/util/ApiService.java
@@ -5,21 +5,12 @@ import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpClient;
-import java.io.IOException;
+import java.net.http.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class ApiService {
-
-    public static void main(String[] args) {
-        searchProduct("Orange Juice",1);
-    }
-
     public static ArrayList<String> searchProduct(String searchString, int page) {
         String searchRequest = "https://world.openfoodfacts.net/api/v2/search?categories_tags_en=" + encodeValue(searchString) + "&fields=code&page_size=100&page="+page+"&sort_by=popularity_key&countries_tags_en=denmark";
         HttpClient client = HttpClient.newHttpClient();
@@ -46,8 +37,8 @@ public class ApiService {
             return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException e) {
             //todo: not this
-            System.out.println(e);
+            TextUI.displayMsg(e.getMessage());
         }
-        return null;
+        return "";
     }
 }
