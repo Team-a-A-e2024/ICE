6b5e21bff5553bb34eabfb2d48587c55f8fe3fd0
Frederik-BipBop <Bastiansen44@gmail.com>
2024-12-11 12:34:32 +0100

    Creating the appservice app - Made a searchproduct method Created an unittest for searchproduct

--- src/main/java/AppService.java
+++ src/main/java/AppService.java
@@ -1,21 +1,44 @@
+import Model.Product;
+import Model.User;
+import Persistens.PoductRepo;
+import util.TextUI;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AppService {
     private List<String> products;
+    User user;
+    private PoductRepo poductRepo;
 
-    public AppService() {
+    public void setProductList(ArrayList<Product> productList) {
+        this.productList = productList;
+    }
+
+    private ArrayList<Product> productList;
+
+    public AppService(PoductRepo poductRepo) {
+        this.poductRepo = poductRepo;
         products = new ArrayList<>();
     }
 
-    public List<String> searchProducts(String searchTerm) {
+    public ArrayList<Product> searchProducts() {
+        ArrayList<Product> results = new ArrayList<>();
+        TextUI.displayMsg("Please enter the product you wish to search");
+        String productName = TextUI.promptText("Search for a product");
 
-        List<String> results = new ArrayList<>();
-        for (String product : products) {
-            if (product.toLowerCase().contains(searchTerm.toLowerCase())) {
-                results.add(product);
+        for (Product p : productList) {
+            if (p.getName().toLowerCase().contains(productName.toLowerCase())) {
+                results.add(p);
             }
         }
-        return results;
+        if (productName.equalsIgnoreCase("x")) {
+            TextUI.displayMsg("You decided not to search, closing searching..");
+            return results;
+        }
+        if (results.isEmpty()) {
+            TextUI.displayMsg(productName + " not found, try again");
+            return searchProducts();
+        }
+            return results;
     }
 }
---
+++ src/test/java/TestAppService.java
@@ -0,0 +1,87 @@
+import Model.Product;
+import Persistens.PoductRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.TextUI;
+import java.util.ArrayList;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class AppServiceTest {
+    private Scanner mockScanner;
+    private PoductRepo mockPoductRepo;
+
+@BeforeEach
+void setup(){
+
+    mockScanner = mock(Scanner.class);
+    TextUI.setScanner(mockScanner);
+    mockPoductRepo = mock(PoductRepo.class);
+
+
+}
+@Test
+    void testAppService() {
+
+    // arrange
+    when(mockScanner.nextLine()).thenReturn("Coca-Cola");
+    Product product = new Product("Coca-Cola",330,120);
+    ArrayList<Product> nyCocaCola = new ArrayList<Product>();
+    nyCocaCola.add(product);
+    // act
+    AppService appservice = new AppService(mockPoductRepo);
+    appservice.setProductList(nyCocaCola);
+    ArrayList<Product> actual = appservice.searchProducts();
+    //assert
+    assertEquals(product.getName(), actual.get(0).getName());
+
+    }
+
+@Test
+void testShortCutSearching() {
+
+    // arrange
+    when(mockScanner.nextLine()).thenReturn("Coca");
+    Product product = new Product("Coca-Cola",330,120);
+    ArrayList<Product> nyCocaCola = new ArrayList<Product>();
+    nyCocaCola.add(product);
+    // act
+    AppService appservice = new AppService(mockPoductRepo);
+    appservice.setProductList(nyCocaCola);
+    ArrayList<Product> actual = appservice.searchProducts();
+    //assert
+    assertEquals(product.getName(), actual.get(0).getName());
+
+}
+@Test
+    void testProductNotFound() {
+
+        // arrange
+        when(mockScanner.nextLine()).thenReturn("mælk", "Coca-Cola");
+        Product product = new Product("Coca-Cola",330,120);
+        ArrayList<Product> nyCocaCola = new ArrayList<Product>();
+        nyCocaCola.add(product);
+        // act
+        AppService appservice = new AppService(mockPoductRepo);
+        appservice.setProductList(nyCocaCola);
+        ArrayList<Product> actual = appservice.searchProducts();
+        //assert
+        assertEquals(product.getName(), actual.get(0).getName());
+
+    }
+    @Test
+    void testClosingSearching() {
+        when(mockScanner.nextLine()).thenReturn("x");
+        // act
+        AppService appservice = new AppService(mockPoductRepo);
+        appservice.setProductList(new ArrayList<Product>());
+        ArrayList<Product> actual = appservice.searchProducts();
+        //assert
+        assertEquals(0, actual.size());
+
+    }
+}
+
+
+
