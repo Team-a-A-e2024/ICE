3f3369d34d461eedd823e6ba83d4edde7075a148
Iirne <emil@caerli.dk>
2024-12-11 12:31:34 +0100

    added static bmi calculator

--- src/main/java/util/HealthCalculator.java
+++ src/main/java/util/HealthCalculator.java
@@ -6,7 +6,7 @@ public class HealthCalculator {
 
     //weight is in KG and height is in CM
     public static float bmiCalculator(float weight, float height){
-        if (height > 0 || weight > 0){
+        if (height > 0 && weight > 0){
             return (weight / height / height) * 10000;
         }
         return 0;
