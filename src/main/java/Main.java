071478ab907ce1687df917eaefa1ac101a1e1fcf
AlissaStorm <Alissaalidib@hotmail.com>
2024-12-12 13:33:07 +0100

    Making the displayNutrition. TotalSum nutrition for a specific dish, display all dishes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import Model.Dish;
+import Model.Product;
+import Persistens.DishRepo;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Product> products = new ArrayList<>();
+        Dish dish = new Dish("Pandekage",400,200, products);
+        products.add(new Product("Mel","0203",400,400,400,400,400,4));
+        products.add(new Product("Sukker","0203",400,400,400,400,400,4));
+        products.add(new Product("Vanilje","0203",400,400,400,400,400,4));
+        DishRepo.saveDish(dish);
+        dish.displayNutrtion();
+
 
     }
 }
--- src/main/java/Model/Dish.java
+++ src/main/java/Model/Dish.java
@@ -1,5 +1,10 @@
 package Model;
 
+import Persistens.DishProductRepo;
+import Persistens.DishRepo;
+import util.TextUI;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dish {
@@ -23,6 +28,7 @@ public class Dish {
     public double getDishWeight() {
         return dishWeight;
     }
+
     //TODO Make the method to calculate calories of products based on weight!!!
     public int getDishCalories() {
         return dishCalories;
@@ -65,4 +71,67 @@ public class Dish {
     public int getId() {
         return id;
     }
+
+    public static void displayNutritionForAllDishes() {
+        ArrayList<Dish> loadedDishes = DishRepo.loadDish();
+        System.out.println("Loaded dishes:");
+        for (Dish d : loadedDishes) {
+            System.out.println(d);
+        }
+    }
+
+    public void displayNutritionForSpecificDish() {
+        //     Retrieve products for a specific dish
+        ArrayList<Dish> loadedDishes = DishRepo.loadDish();
+
+        if (!loadedDishes.isEmpty()) {
+            int dishId = loadedDishes.get(0).getId(); // Assuming Dish has an `id` field
+            ArrayList<Product> loadedProducts = DishProductRepo.getProductsForDish(dishId);
+            TextUI.displayMsg("Products for dish " + loadedDishes.get(0).getName() + ":");
+            for (Product p : loadedProducts) {
+                System.out.println(p);
+            }
+        }
+
+    }
+
+    public void displayTotalNutritionForSpecificDish() { //set a users choice parameter
+        // Load all dishes
+        ArrayList<Dish> loadedDishes = DishRepo.loadDish();
+
+        if (!loadedDishes.isEmpty()) {
+            // Choose a dish to calculate nutrition for (e.g., the first dish)
+            Dish selectedDish = loadedDishes.get(0); // Replace with user selection if needed
+            int dishId = selectedDish.getId();
+
+            // Retrieve products for the selected dish
+            ArrayList<Product> loadedProducts = DishProductRepo.getProductsForDish(dishId); //TODO Replace dishId or make a method where we can see dishId before selecting a dish
+
+            double totalCalories = 0;
+            int totalCarbs = 0;
+            int totalSugar = 0;
+            int totalProtein = 0;
+            int totalFat = 0;
+
+            for (Product product : loadedProducts) {
+                totalCalories += product.getCalorie();
+                totalCarbs += product.getCarb();
+                totalSugar += product.getSugar();
+                totalProtein += product.getProtein();
+                totalFat += product.getFat();
+            }
+
+            // Display results
+            System.out.println("Nutritional values for dish: " + selectedDish.getName());
+            System.out.println("Total Calories: " + totalCalories);
+            System.out.println("Total Carbs: " + totalCarbs + "g");
+            System.out.println("Total Sugar: " + totalSugar + "g");
+            System.out.println("Total Protein: " + totalProtein + "g");
+            System.out.println("Total Fat: " + totalFat + "g");
+        } else {
+            System.out.println("No dishes available.");
+        }
+    }
+
 }
+
