5c954b32b4afc9558951d7f3a2ad3f0077c2bd3b
Thomas Atchapero <t.atchapero@gmail.com>
2024-12-16 11:43:03 +0100

    Added method for searching product by code.

--- src/main/java/util/ApiService.java
+++ src/main/java/util/ApiService.java
@@ -3,6 +3,7 @@ package util;
 import Model.Product;
 import Persistens.ProductRepo;
 import com.sun.jdi.DoubleValue;
+import enums.DishCategory;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.UnsupportedEncodingException;
@@ -12,6 +13,7 @@ import java.net.URLEncoder;
 import java.net.http.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ApiService {
@@ -39,9 +41,50 @@ public class ApiService {
                 "&sort_by=popularity_key";
 
         String responseBody = GET(searchRequest);
-      
 
         return productParser(responseBody);
+    }
+
+    public static Product searchProductByCode(String barcode) {
+        String searchRequest = baseUrl + "/api/v3/product/" + barcode + ".json";
+
+        String responseBody = GET(searchRequest);
+
+        return parseSingleProduct(responseBody);
+    }
+
+    private static Product parseSingleProduct(String responseBody) {
+        ApiService.products = ProductRepo.loadProducts();
+        JSONObject obj = new JSONObject(responseBody);
+        String code = obj.getString("code");
+        JSONObject jsonProduct = obj.getJSONObject("product");
+
+        try {
+            String productName = jsonProduct.getString("product_name");
+            if (productName == null || productName.isEmpty() || productName.isBlank()){
+                return null;
+            }
+            else if (!isProductSaved(code)) {
+                JSONObject jsonNutriments = jsonProduct.getJSONObject("nutriments");
+                Product product = new Product(
+                        jsonProduct.getString("product_name"),
+                        jsonProduct.getString("code"),
+                        jsonProduct.getBigDecimal("product_quantity").doubleValue(),
+                        jsonNutriments.getBigDecimal("energy-kcal_100g").intValue(),
+                        jsonNutriments.getBigDecimal("carbohydrates_100g").intValue(),
+                        jsonNutriments.getBigDecimal("sugars_100g").intValue(),
+                        jsonNutriments.getBigDecimal("proteins_100g").intValue(),
+                        jsonNutriments.getBigDecimal("fat_100g").intValue()
+                );
+
+                ProductRepo.saveProduct(product);
+
+                return product;
+            }
+        } catch (Exception e) {
+            TextUI.displayMsg("Could not parse product");
+        }
+        return null;
     }
 
     private static List<Product> productParser(String responseBody) {
@@ -54,29 +97,29 @@ public class ApiService {
 
         for (int i = 0; i < arr.length(); i++)
         {
-            String code = arr.getJSONObject(i).getString("code");
+            String code = obj.getString("code");
 
             try {
-                String productName = arr.getJSONObject(i).getString("product_name");
+                String productName = obj.getString("product_name");
                 if (productName == null || productName.isEmpty() || productName.isBlank()){
                     continue;
                 }
                 else if (!isProductSaved(code)) {
                     Product product = new Product(
-                            arr.getJSONObject(i).getString("product_name"),
-                            arr.getJSONObject(i).getString("code"),
-                            arr.getJSONObject(i).getBigDecimal("product_quantity").doubleValue(),
-                            arr.getJSONObject(i).getBigDecimal("energy-kcal_100g").intValue(),
-                            arr.getJSONObject(i).getBigDecimal("carbohydrates_100g").intValue(),
-                            arr.getJSONObject(i).getBigDecimal("sugars_100g").intValue(),
-                            arr.getJSONObject(i).getBigDecimal("proteins_100g").intValue(),
-                            arr.getJSONObject(i).getBigDecimal("fat_100g").intValue()
+                            obj.getString("product_name"),
+                            obj.getString("code"),
+                            obj.getBigDecimal("product_quantity").doubleValue(),
+                            obj.getBigDecimal("energy-kcal_100g").intValue(),
+                            obj.getBigDecimal("carbohydrates_100g").intValue(),
+                            obj.getBigDecimal("sugars_100g").intValue(),
+                            obj.getBigDecimal("proteins_100g").intValue(),
+                            obj.getBigDecimal("fat_100g").intValue()
                     );
 
                     ProductRepo.saveProduct(product);
                 }
             } catch (Exception e) {
-
+                TextUI.displayMsg("Could not parse product");
             }
         }
 
